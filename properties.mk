5c0bfbf9e3b024688ab11fa302e0994ffa6b01c6
Fraaxius <franthekillaz@gmail.com>
2024-08-15 00:18:24 -0400

    astro: Initial device tree
    * Based on https://github.com/LineageOS/android_device_motorola_liber
    * Imported blobs from astro_retail-user-11-RPLS31.Q2-63-10-2-9-c6698-release-keys
    
    Signed-off-by: Fraaxius <franthekillaz@gmail.com>

---
+++ properties.mk
@@ -0,0 +1,29 @@
+#
+# Properties for liber
+#
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/General_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/Global_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/Handset_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb \
+    persist.vendor.audio.calfile7=/vendor/etc/acdbdata/Codec_cal.acdb
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.physical.num=5
+
+# Chipset
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI \
+    ro.soc.model=SDM710
+
+# Sensor
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.vendor.sensors.mot_ltv=true \
+    persist.vendor.sensors.hal_trigger_ssr=true \
+    persist.vendor.sensors.odl.adsp=true
